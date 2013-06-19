5e76669f6f0767f184d4442dcbf27d1ec55529f1
tommyb67 <tommybrennan@gmail.com>
2013-06-19 16:49:36 -0400

    first draft of assign groups

---
+++ assign_groups.rb
@@ -0,0 +1,17 @@
+students = ["Alan Appelstein", "Alice Mottola", "Alina Guzman", "andrew silbersmith", "Anuj Jhunjhunwala", "Ben Israeli", "Beryl Schragger", "Chris Clearfield", "Christine Coulter", "Christopher Huyett", "Christopher Wright", "Corbin Sykes", "Eleyna Whittingham", "Eric Gong", "Jamal Powell", "Jason Darcy", "Kevon Cheung", "Mike Mayer", "Mike Vanger", "Nick Lombardi", "Phillip Lamplugh", "Teddy Cleveland", "Thomas Yu", "Tim Blonski", "Tom Brennan", "Tricia Dougals", "Wesley Carr", "Zachariah Reitano"]
+
+#students = [1,2,3,4]
+
+#puts students.each {|x| puts if x==0}
+
+students.each_with_index do|student, x|
+  puts student if x%4==0
+end
+
+#groups=[]
+
+#students.each_with_index do|student, x|
+ #student if x%2==0
+#end
+
+#puts groups
